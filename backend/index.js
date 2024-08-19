84856176ec7af0d1d71a307dd82e985ed21afafb
ash-github <137061875+ash-github@users.noreply.github.com>
2024-08-19 19:50:48 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -5,7 +5,7 @@ const path=require("path")
 connectToMongo()
 
 const app=express()
-const port=3000
+const port=8000
 app.use(express.json());
 const _dirname=path.dirname("")
 const buildpath = path.join(_dirname,"../build")
